5ded1e4f86cfcfe885c728c24f4f21f2f31ee178
Jefferson Yuri Cunha Burk <yuriburk@gmail.com>
2020-04-18 14:10:20 -0300

    feature: Created migrations for features

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,55 @@
-// import AppError from '../errors/AppError';
+import { getRepository, Repository } from 'typeorm';
 
+import AppError from '../errors/AppError';
+import Category from '../models/Category';
 import Transaction from '../models/Transaction';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  private categoriesRepository: Repository<Category>;
+
+  private transactionsRepository: TransactionsRepository;
+
+  constructor() {
+    this.categoriesRepository = getRepository(Category);
+    this.transactionsRepository = new TransactionsRepository();
+  }
+
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    let categorySaved = await this.categoriesRepository.findOne({
+      where: { title: category },
+    });
+
+    if (!categorySaved) {
+      categorySaved = this.categoriesRepository.create({
+        title: category,
+      });
+
+      await this.categoriesRepository.save(categorySaved);
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+      category_id: categorySaved.id,
+    });
+
+    await this.transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
